f666bc61fb875d9fed409f477e021d174638a34d
brem02 <122722304+brem02@users.noreply.github.com>
2023-06-13 11:34:37 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,4 +1,9 @@
 provider "azurerm" {
+  # version         = "~> 2.29.0"
+  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
   features {}
 }
 terraform {
