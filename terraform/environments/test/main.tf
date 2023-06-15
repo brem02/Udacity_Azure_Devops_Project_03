7f4190aae7b2a9746975705752f9c88f6ee72df7
brem02 <122722304+brem02@users.noreply.github.com>
2023-06-16 01:20:34 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -61,5 +61,6 @@ module "vm" {
   subnet_id            = module.network.subnet_id_test
   public_ip_address_id = module.publicip.public_ip_address_id 
   admin_username       = var.admin_username
+  admin_password       = var.admin_password  
   prefix               = var.prefix
 }
