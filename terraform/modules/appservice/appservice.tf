3c3602bba818ed6d6a542bb0a27d6059d146770f
brem02 <122722304+brem02@users.noreply.github.com>
2023-06-16 17:37:26 +0200

    Update appservice.tf

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,5 +1,5 @@
 resource "azurerm_app_service_plan" "test" {
-  name                = "${var.application_type}-${var.resource_type}"
+  name                = "${var.application_type}-${var.resource_type}-Plan"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 
